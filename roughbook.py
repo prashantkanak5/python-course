209e1ab05745f70250b56e381c7ded213bf37922
Prashant Kanak <prashant.kanak238@gmail.com>
2026-02-16 00:10:47 +0530

    learning part 1

---
+++ roughbook.py
@@ -0,0 +1,14 @@
+#Added name 
+name = "Alice"
+#printing name
+print("Hello", name)
+
+print("---")
+print("Output should be: \nName: \t John \nAge:\t 30\nHe said, \"I'm learning Python.")
+
+print("---")
+print("Apple", "Banana", "Mango", sep=" | ", end="<End>")
+print("")
+print("---")
+print("Welcome to python!\n \tLearn to code \n\t\tHave fun! ")
+print("---")
---
+++ varanddatatype.py
@@ -0,0 +1,10 @@
+var1 = "Prashant Kanak"
+var2 = 100
+var3 = 0.5
+var4 = False
+
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
